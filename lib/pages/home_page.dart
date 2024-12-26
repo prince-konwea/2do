e18f7c08a187f052702db55f7f75d1d18dd1a155
prince-konwea <konweaprince09@gmail.com>
2024-12-26 02:20:08 +0100

    feat: modify padding

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,10 +19,23 @@ class HomePage extends StatelessWidget {
         body: ListView.builder(
             itemCount: todoList.length,
             itemBuilder: (BuildContext context, index) {
-              return Container(
-                  child: Text(
-                todoList[index][0],
-              ));
+              return Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                    left: 20,
+                    right: 20,
+                    bottom: 0,
+                  ),
+                  child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Text(todoList[index][0],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ))));
             }));
   }
 }
